217e7bfab11065a781ecea7f5a17980700880f6c
rolpro <nationalroland@gmail.com>
2020-11-06 19:32:15 +0100

    Rot. Sonst nix.

--- build/ios/StyleDictionaryColor.m
+++ build/ios/StyleDictionaryColor.m
@@ -3,7 +3,7 @@
 // StyleDictionaryColor.m
 //
 // Do not edit directly
-// Generated on Thu, 15 Oct 2020 16:55:25 GMT
+// Generated on Fri, 06 Nov 2020 18:28:02 GMT
 //
 
 #import "StyleDictionaryColor.h"
@@ -21,31 +21,7 @@
 
   dispatch_once(&onceToken, ^{
     colorArray = @[
-rgba(255, 230, 0, 1),
-rgba(112, 141, 242, 1),
-rgba(4, 74, 255, 1),
-rgba(64, 255, 186, 1),
-rgba(64, 255, 186, 1),
-rgba(0, 0, 0, 0.1),
-rgba(4, 74, 255, 1),
-rgba(64, 223, 80, 1),
-rgba(52, 86, 175, 1),
-rgba(255, 184, 0, 1),
-rgba(255, 184, 0, 1),
-rgba(255, 255, 255, 1),
-rgba(255, 255, 255, 1),
-rgba(207, 48, 48, 1),
-rgba(255, 255, 255, 1),
-rgba(74, 79, 204, 1),
-rgba(255, 255, 255, 1),
-rgba(255, 184, 0, 1),
-rgba(255, 138, 0, 1),
-rgba(255, 46, 0, 1),
-rgba(255, 0, 0, 1),
-rgba(0, 0, 0, 0.25),
-rgba(0, 0, 0, 0.25),
-rgba(0, 0, 0, 1),
-rgba(0, 0, 0, 0.25)
+rgba(252, 5, 83, 1)
     ];
   });
 
