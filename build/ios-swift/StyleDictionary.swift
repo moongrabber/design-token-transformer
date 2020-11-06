d4c1b580d677e302b9ac6a99b96e88efb63695ac
Automated Publisher <actions@users.noreply.github.com>
2020-11-06 20:36:47 +0000

    Automated publish: Fri Nov  6 20:36:47 UTC 2020 b6004e330db8cac462a8cada97ad92756d54f8bd

--- build/ios-swift/StyleDictionary.swift
+++ build/ios-swift/StyleDictionary.swift
@@ -3,7 +3,7 @@
 // StyleDictionary.swift
 //
 // Do not edit directly
-// Generated on Fri, 06 Nov 2020 20:32:28 GMT
+// Generated on Fri, 06 Nov 2020 20:36:46 GMT
 //
 
 
--- build/ios/StyleDictionarySize.h
+++ build/ios/StyleDictionarySize.h
@@ -2,7 +2,7 @@
 // StyleDictionarySize.h
 //
 // Do not edit directly
-// Generated on Fri, 06 Nov 2020 20:32:28 GMT
+// Generated on Fri, 06 Nov 2020 20:36:46 GMT
 //
 
 #import <Foundation/Foundation.h>
